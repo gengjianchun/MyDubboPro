08437825db3e9f596659b88187c429ff0d987ae8
gengjianchun <geng.jianchun@163.com>
2018-01-27 18:40:19 +0800

    修改provider启动方法

--- provider/src/main/java/DubboStarter.java
+++ provider/src/main/java/DubboStarter.java
@@ -1,14 +1,25 @@
+import java.io.IOException;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class DubboStarter {
-    public static void main(String[] args) throws InterruptedException {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+    public static void main(String[] args) throws Exception {
+   /*     ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         context.start();
         synchronized (DubboStarter.class) {
             while(true){
                 DubboStarter.class.wait();
             }
-        }
+        }*/
+        
+        //官方示例代码启动方式
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        context.start();
+
+        System.in.read(); // press any key to exit
+        
+        
     }
 }
